9c3ba07ff7ba815a0b0dc9ee2a3ccce379bb179e
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-11-29 11:28:53 +0100

    Setup AdminController

--- TurboKart.Presentation.Websites.TurboKartBookingManagement/Controllers/AdminController.cs
+++ TurboKart.Presentation.Websites.TurboKartBookingManagement/Controllers/AdminController.cs
@@ -4,14 +4,30 @@ namespace TurboKart.Presentation.Websites.TurboKartBookingManagement.Controllers
 {
     public class AdminController : Controller
     {
+        // TurboKart Booking Mangement 5, 6 and 7
         public IActionResult Index()
         {
             return View();
         }
 
+        // TurboKart Booking Mangement 8
         public IActionResult LogIn()
         {
             return View();
         }
+
+        // TurboKart Booking Mangement 2, 3, and 4
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        // TurboKart Booking Mangement  1
+        public IActionResult NewBooking()
+        {
+            return View();
+        }
+
+
     }
 }
