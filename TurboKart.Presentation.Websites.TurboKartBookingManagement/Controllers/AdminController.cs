6ce37bc7e4de671187f8926fda142820367ee65d
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-11-29 11:20:46 +0100

    TurboKartBookingManagement MVC File setup

---
+++ TurboKart.Presentation.Websites.TurboKartBookingManagement/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TurboKart.Presentation.Websites.TurboKartBookingManagement.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ TurboKart.Presentation.Websites.TurboKartBookingManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TurboKart.Presentation.Websites.TurboKartBookingManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
