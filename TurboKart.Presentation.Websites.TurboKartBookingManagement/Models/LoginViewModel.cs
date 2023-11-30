671ad16988f25fe067cb23c17e1476fa63a40522
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-11-30 13:27:32 +0100

    Added LoginViewModel and temporay Validation for Username and Password

---
+++ TurboKart.Presentation.Websites.TurboKartBookingManagement/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TurboKart.Presentation.Websites.TurboKartBookingManagement.Models
+{
+	public class LoginViewModel
+	{
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+	}
+}
