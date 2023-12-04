928a919bf02b78392a940709e5640ae368f8aa9d
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-12-04 10:33:54 +0100

    Added HttpPOST Method NewCustomer in CustomerController

--- TurboKart.Presentation.Apis.TurboKartAPI/Controllers/CustomerController.cs
+++ TurboKart.Presentation.Apis.TurboKartAPI/Controllers/CustomerController.cs
@@ -2,63 +2,76 @@
 
 namespace TurboKart.Presentation.Apis.TurboKartAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CustomerController : ControllerBase
-    {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
 
-        private readonly ICustomerUseCase customerUseCase;
+		private readonly ICustomerUseCase customerUseCase;
 
-        public CustomerController(ICustomerUseCase customerUseCase)
-        {
-            this.customerUseCase = customerUseCase;
-        }
+		public CustomerController(ICustomerUseCase customerUseCase)
+		{
+			this.customerUseCase = customerUseCase;
+		}
 
-        [HttpGet("all")]
-        public ActionResult<IEnumerable<Customer>> GetAllCustomers()
-        {
-            return Ok(customerUseCase.GetAllCustomers());
-        }
+		[HttpGet("all")]
+		public ActionResult<IEnumerable<Customer>> GetAllCustomers()
+		{
+			return Ok(customerUseCase.GetAllCustomers());
+		}
 
-        [HttpGet("{id}")]
-        public ActionResult<Customer> GetSingleCustomer(int id)
-        {
-            var result = customerUseCase.GetSingleCustomer(id);
-            if (result == null)
-                return NotFound("No customer found with that ID");
+		[HttpGet("{id}")]
+		public ActionResult<Customer> GetSingleCustomer(int id)
+		{
+			var result = customerUseCase.GetSingleCustomer(id);
+			if (result == null)
+				return NotFound("No customer found with that ID");
 
-            return Ok(result);
-        }
+			return Ok(result);
+		}
 
-        [HttpPut("update")]
-        public ActionResult Update(Customer customer)
-        {
-            try
-            {
-                customerUseCase.Update(customer);
-                return Ok();
-            }
-            catch (Exception)
-            {
-                return BadRequest();
+		[HttpPut("update")]
+		public ActionResult Update(Customer customer)
+		{
+			try
+			{
+				customerUseCase.Update(customer);
+				return Ok();
+			}
+			catch (Exception)
+			{
+				return BadRequest();
 
-            }
-        }
+			}
+		}
 
-        [HttpDelete("delete")]
-        public ActionResult Delete(Customer customer)
-        {
-            try
-            {
-                customerUseCase.Delete(customer);
-                return Ok();
-            }
-            catch (Exception)
-            {
-                return BadRequest();
+		[HttpDelete("delete")]
+		public ActionResult Delete(Customer customer)
+		{
+			try
+			{
+				customerUseCase.Delete(customer);
+				return Ok();
+			}
+			catch (Exception)
+			{
+				return BadRequest();
 
-            }
-        }
+			}
+		}
 
-    }
+		[HttpPost("new")]
+		public ActionResult<Customer> NewCustomer(Customer customer)
+		{
+			try
+			{
+				return Ok(customerUseCase.NewCustomer(customer));
+			}
+			catch (Exception e)
+			{
+				return BadRequest();
+			}
+		}
+
+	}
 }
